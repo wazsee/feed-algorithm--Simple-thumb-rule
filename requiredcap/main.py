311f068faadc04ec57c28700ff03c7fdd76fa15d
Satish19 <92371111+wazsee@users.noreply.github.com>
2022-06-02 22:06:15 +0530

    Add files via upload

---
+++ requiredcap/main.py
@@ -0,0 +1,30 @@
+from urllib import response
+from fastapi import FastAPI
+from pydantic import BaseModel
+from factory import connector1
+import uvicorn
+import pandas as pd
+import asyncio
+
+api = FastAPI()
+class Greendryfodder(BaseModel):
+    Cattle_Weight:int
+    Milking_ltrs:int
+    
+
+@api.get('/api/')
+async def hello_home():
+    return "Welcome to the feed API!"
+    
+@api.post('/api/GDC')
+async def greendry(request: Greendryfodder):
+    # print(request.dict())
+    response_json = connector1.GDC(request.dict())
+    # print(response_json)
+    return response_json
+    # return {}
+
+
+loop = asyncio.get_event_loop()
+result = loop.run_until_complete(coro)
+#server = uvicorn.run(api)
